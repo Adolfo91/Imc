88822ae7b09033ace28543db0468c81219c4d504
KakoF <kakoferrare87@gmail.com>
2016-07-05 21:22:46 -0300

    Atualização de Header e Footer

--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,12 @@
       </button>
       <a class="navbar-brand" href="#">Projeto IMC</a>
     </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">O que é IMC <span class="sr-only">(current)</span></a></li>
+        <li><a href="pages/calculo.php">Calcule seu IMC</a></li>
+</ul>
+    <!-- Collect the nav links, forms, and other content for toggling -->
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Nosso Contato <span class="caret"></span></a>
