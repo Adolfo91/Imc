6c63963c0033c2a96bc379f5bceb564890c0632e
KakoF <kakoferrare87@gmail.com>
2016-07-12 20:01:03 -0300

    Atualização

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,24 @@
-<nav class="navbar navbar-default" role="navigation">
+<nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ImcNavBar" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="index.php?page=about">Projeto IMC</a>
     </div>
-    <div class="collapse navbar-collapse" id="ImcNavBar">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-      <li class=<?php echo isset($_GET['page']) ? $_GET['page'] == 'about' ? 'active': '' : 'active' ?>><a href="index.php?page=about">O que é IMC <span class="sr-only">(current)</span></a></li>
+         <li class=<?php echo isset($_GET['page']) ? $_GET['page'] == 'about' ? 'active': '' : 'active' ?>><a href="index.php?page=about">O que é IMC <span class="sr-only">(current)</span></a></li>
         <li class=<?php echo isset($_GET['page']) ? $_GET['page'] == 'calculo' ? 'active': '' : '' ?>><a href="index.php?page=calculo">Calcule seu IMC</a></li>
-</ul>
-    <!-- Collect the nav links, forms, and other content for toggling -->
+      </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Nosso Contato <span class="caret"></span></a>
+         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Nosso Contato <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="http://www.facebook.com/adolfo.pesoti" target="_blank">Adolfo</a></li>
+           <li><a href="http://www.facebook.com/adolfo.pesoti" target="_blank">Adolfo</a></li>
             <li><a href="http://www.facebook.com/kako.ferrare" target="_blank">Kako</a></li>
           </ul>
         </li>
